31a2537b3154ba4a48bdd9275dacc41461f42cdb
OoL33 <alanalee@tutanota.com>
2023-08-14 15:42:12 -0400

    Remove all files from gh-pages branch

--- src/components/PortfolioContainer.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const PortfolioContainer = ({ children }) => {
-  return <div style={{ display: "flex", minHeight: "100vh" }}>{children}</div>;
-};
-
-export default PortfolioContainer;
